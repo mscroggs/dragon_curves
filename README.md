d05d7af0a3d153f631e90408ce7f172504556b0c
MScroggs <matthew.w.scroggs@gmail.com>
2016-03-25 16:15:30 +0000

    Added return_position

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 DRAGON CURVE
 ============
 This repository contains Python code to draw dragon curves.
+
+The file `core.py` contains the functions which generate the curves.
+
+Run `draw_a_curve.py` to draw a single curve. Run `make_a_tile.py` to make a tileable
+image.
--- core.py
+++ core.py
@@ -9,10 +9,10 @@ def dragon(n):
     return prev + [1] + prev_s
 
 
-def dragon_arc(n,xst=0,yst=0,dir_deg=None,dir_rad=None,angle_deg=None,angle_rad=None):
+def dragon_arc(n,xst=0,yst=0,dir_deg=None,dir_rad=None,angle_deg=None,angle_rad=None,return_position=False):
     """Returns the svgwrite commands to draw an order n dragon curve."""
     from math import pi,sqrt,cos,sin
-
+    pos = [xst,yst]
     angle = pi/2
     if angle_deg is not None:
         angle = angle_deg*pi/180
@@ -36,12 +36,17 @@ def dragon_arc(n,xst=0,yst=0,dir_deg=None,dir_rad=None,angle_deg=None,angle_rad=
             dir -= angle/2
         my_d += " "
         my_d += str(length*cos(dir))
+        pos[0] += length*cos(dir)
         my_d += ","        
         my_d += str(-length*sin(dir))
+        pos[1] -= length*sin(dir)
         if l==0:
             dir += angle/2
         else:
             dir -= angle/2
+
+    if return_position:
+        return my_d,pos
 
     return my_d
 
